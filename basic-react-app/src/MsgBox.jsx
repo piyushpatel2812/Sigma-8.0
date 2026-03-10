0bc4ee42ab35e7b57dd4a5c7a8fc496d5fe6e103
piyushpatel2812 <piyushpatel281205@gmail.com>
2026-03-10 08:27:16 +0530

    style comonent

---
+++ basic-react-app/src/MsgBox.jsx
@@ -0,0 +1,6 @@
+export default function MsgBox({userName,textcolor}){
+    let style = {backgroundColor:"pink"};
+    return (
+        <h1> hello,{userName} </h1>
+    )
+}
--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,17 +1,34 @@
 import "./Product.css";
 
+// function Product({title,price,features,features2}){
+//     return (
+//         <div className="Product">
+//             <h3>{title}</h3>
+//             {/* <h5>price : {price+15000}</h5> */}
+//             <h5>price : {price}</h5>
+//             <p>{features}</p>
+//             {/* <p>{features2?.a}</p> */}
+//             {price >30000 ? <p>discount of 5%</p> : null}
+           
+//         </div>
+//     );
+// }
+
 function Product({title,price,features,features2}){
+    let isDiscount = price > 3000;
+    let styles = {backgroundColor: isDiscount ? "pink" :""};
     return (
-        <div className="Product">
+        <div className="Product"  >
             <h3>{title}</h3>
-            {/* <h5>price : {price+15000}</h5> */}
+          
             <h5>price : {price}</h5>
-            <p>{features}</p>
-            <p>{features2?.a}</p>
-            {/* <p>{features2?.b}</p>
-            <p>{features2?.c}</p> */}
+            <p  style={styles}>{features}</p>
+     
+            {isDiscount ? <p>discount of 5%</p> : null}
+           
         </div>
     );
 }
+
 
 export default Product;
